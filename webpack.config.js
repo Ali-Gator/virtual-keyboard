a2a3b6a7f5afc30888889c63287631ec6b3aaa43
Oleg Gordienov <ogordienov@yandex.ru>
2022-05-03 21:18:20 +0200

    feat: add dev environment

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    mode: 'development',
+    devServer: {
+        static: './dist',
+        compress: true,
+        port: 8080,
+        watchFiles: ['src/**/*']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
+        ]
+    }
+};
